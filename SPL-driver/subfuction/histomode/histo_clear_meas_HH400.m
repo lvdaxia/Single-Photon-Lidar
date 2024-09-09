b704a337a26df2a198f5e2eb3fd2dee91f8abb2b
lvdaxia <49050899+lvdaxia@users.noreply.github.com>
2024-09-09 18:05:11 +0800

    Delete SPL-driver directory

--- SPL-driver/subfuction/histomode/histo_clear_meas_HH400.m
+++
@@ -1,6 +0,0 @@
-ret = calllib('HHlib', 'HH_ClearHistMem', dev(1));    
-if (ret<0)
-    fprintf('\nHH_ClearHistMem error %ld. Aborted.\n', ret);
-    closedev;
-    return;
-end
--- SPL-driver/subfuction/histomode/histo_start_meas_HH400.m
+++
@@ -1,7 +0,0 @@
-ret = calllib('HHlib', 'HH_StartMeas', dev(1),Tacq); 
-if (ret<0)
-    fprintf('\nHH_StartMeas error %ld. Aborted.\n', ret);
-    closedev;
-    return;
-end;
-fprintf('\nMeasuring for %1d milliseconds...',Tacq);
